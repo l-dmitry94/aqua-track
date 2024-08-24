35ef91d1e2a90147c973893e111b68a521995557
Dmytro <64677316+l-dmitry94@users.noreply.github.com>
2024-08-24 10:23:29 +0300

    added mongodb connection

---
+++ models/user.ts
@@ -0,0 +1,17 @@
+import mongoose, { models, Schema } from 'mongoose';
+
+const userSchema = new Schema(
+    {
+        name: { type: String, required: true },
+        email: { type: String, required: true },
+        password: { type: String, required: true },
+    },
+    {
+        timestamps: true,
+        versionKey: false,
+    }
+);
+
+const User = models.User || mongoose.model('User', userSchema);
+
+export default User;
