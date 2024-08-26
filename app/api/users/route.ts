caf73af904aa388291274b65f01edf60ffb3da83
Dmytro <64677316+l-dmitry94@users.noreply.github.com>
2024-08-26 18:18:34 +0300

    Update route.ts

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,8 +1,11 @@
 import { NextResponse } from 'next/server';
 
+import { connectMongoDB } from '@/lib/mongodb';
+
 import { allUsers } from './services';
 
 export const GET = async () => {
+    await connectMongoDB();
     const users = await allUsers();
 
     return NextResponse.json(users);
