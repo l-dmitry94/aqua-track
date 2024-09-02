9172c14b1483b75dd93e64b4b2a76907bb30a423
Liubov <drogaltseva92@gmail.com>
2024-09-02 14:49:55 +0200

    add resp to current, update swagger doc, add route get all users

--- app/api/users/current/route.ts
+++ app/api/users/current/route.ts
@@ -2,6 +2,8 @@ import { NextRequest, NextResponse } from 'next/server';
 
 import { authenticate } from '@/middlewares/authenticate';
 
+import { findUser } from '../services';
+
 export const GET = async (req: NextRequest) => {
     const user = await authenticate(req);
 
@@ -9,13 +11,24 @@ export const GET = async (req: NextRequest) => {
         return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
     }
 
-    const { name, email } = user;
+    const userData = await findUser({ _id: user._id });
+
+    if (!userData) {
+        return NextResponse.json({ message: 'User not found' }, { status: 404 });
+    }
+
+    const { name, email, gender, weight, activeTime, waterIntake, avatar } = userData;
 
     return NextResponse.json(
         {
             user: {
                 name,
                 email,
+                gender,
+                weight,
+                activeTime,
+                waterIntake,
+                avatar,
             },
         },
         { status: 200 }
