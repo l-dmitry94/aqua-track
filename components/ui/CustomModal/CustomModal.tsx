e21fa21cc0bc1fd44829d433a4f15c0e83330f84
PetroBrodetskyi <petro.brodetskyi@gmail.com>
2024-09-03 11:00:17 +0200

    Modal fixed

---
+++ components/ui/CustomModal/CustomModal.tsx
@@ -0,0 +1,59 @@
+import React, { FC, ReactNode } from 'react';
+import { Backdrop, Box, Fade, IconButton, Modal, Typography } from '@mui/material';
+import clsx from 'clsx';
+
+import Button from '@/components/ui/Button/Button';
+import Icon from '@/components/ui/Icon/Icon';
+
+import scss from './CustomModal.module.scss';
+
+interface CustomModalProps {
+    open: boolean;
+    onClose: () => void;
+    title: string;
+    children: ReactNode;
+    variant?: 'primary' | 'secondary';
+}
+
+const CustomModal: FC<CustomModalProps> = ({
+    open,
+    onClose,
+    title,
+    children,
+    variant = 'primary',
+}) => {
+    return (
+        <Modal
+            open={open}
+            onClose={onClose}
+            aria-labelledby="modal-title"
+            aria-describedby="modal-content"
+            closeAfterTransition
+            slots={{ backdrop: Backdrop }}
+            slotProps={{
+                backdrop: {
+                    timeout: 500,
+                },
+            }}
+        >
+            <Fade in={open}>
+                <Box className={clsx(scss.modalBox, scss[variant])}>
+                    <IconButton className={scss.closeButton} onClick={onClose} aria-label="Close">
+                        <Icon className={scss.icon} variant="close" />
+                    </IconButton>
+                    <Typography id="modal-title" className={scss.title} component="h2">
+                        {title}
+                    </Typography>
+                    <div id="modal-content" className={scss.content}>
+                        {children}
+                    </div>
+                    <Button variant="contained" className={scss.button}>
+                        Save
+                    </Button>
+                </Box>
+            </Fade>
+        </Modal>
+    );
+};
+
+export default CustomModal;
