e3b8e72752d57cab5b9ce726e00719ec494cac59
sivlvy <vladislavsidenko@ukr.net>
2024-09-03 14:50:07 +0300

    fix lint

--- app/tracker/page.tsx
+++ app/tracker/page.tsx
@@ -1,7 +1,20 @@
-export interface TrackerPageProps {}
+import React from 'react';
 
-const TrackerPage = ({}: TrackerPageProps) => {
-    return <div>TrackerPage</div>;
+import Settings from '@/components/shared/Settings';
+import TrackerInfo from '@/components/shared/TrackerInfo';
+import { WaterMainInfo } from '@/components/shared/WaterMainInfo';
+import Container from '@/components/ui/Container';
+
+const TrackerPage = () => {
+    return (
+        <div>
+            <Container>
+                <TrackerInfo />
+                <WaterMainInfo />
+                <Settings />
+            </Container>
+        </div>
+    );
 };
 
 export default TrackerPage;
