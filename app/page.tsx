78793088ce8c1f4ba4c8d2a39eede0de80a8a7a3
Dmytro <64677316+l-dmitry94@users.noreply.github.com>
2024-09-04 14:20:54 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,10 @@
 'use client';
 
-import { FC, useState } from 'react';
-
 import Container from '@/components/ui/Container';
-import CustomModal from '@/components/ui/CustomModal';
 
-const HomePage: FC = () => {
-    const [modalIsOpen, setModalIsOpen] = useState(false);
+const HomePage = () => {
     return (
         <Container>
-            <button onClick={() => setModalIsOpen(true)}>Open Modal</button>
-            <CustomModal
-                open={modalIsOpen}
-                title="Hello"
-                onClose={() => setModalIsOpen(false)}
-                profile
-            >
-                My name is Dima
-            </CustomModal>
             <p>HomePage</p>
         </Container>
     );
