9ea63521b879d2b3565028eb00850fce50992dbf
Dmytro <64677316+l-dmitry94@users.noreply.github.com>
2024-09-12 10:36:39 +0300

    get all users

---
+++ app/api/users/route.ts
@@ -0,0 +1,9 @@
+import { NextResponse } from 'next/server';
+
+import prisma from '@/prisma/prisma';
+
+export const GET = async () => {
+    const users = await prisma.user.findMany();
+
+    return NextResponse.json(users);
+};
