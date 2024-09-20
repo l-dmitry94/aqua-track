1ecfd747fa489401aada7612418ac5ef1d625b69
Dmytro <64677316+l-dmitry94@users.noreply.github.com>
2024-09-20 09:37:00 +0300

    update water model

--- app/api/water/create/route.ts
+++ app/api/water/create/route.ts
@@ -1,31 +1,23 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth/next';
 
 import { authOptions } from '@/lib/authOptions';
 import prisma from '@/prisma/prisma';
 
-export const POST = async (req: Request) => {
+export const POST = async (req: NextRequest) => {
     const session = await getServerSession(authOptions);
 
     if (!session) {
         return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
     }
 
-    const { date, volume }: { date: string; volume: number } = await req.json();
-
-    if (!date || volume === undefined) {
-        return NextResponse.json(
-            { message: 'Bad Request: Missing required fields' },
-            { status: 400 }
-        );
-    }
+    const body = await req.json();
 
     try {
         const waterEntry = await prisma.water.create({
             data: {
                 userId: session.user.id,
-                date: new Date(date),
-                volume,
+                ...body,
             },
         });
 
