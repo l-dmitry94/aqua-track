0208910c0be47d30769deb29f20bc7414c4f3c0c
Dmytro <64677316+l-dmitry94@users.noreply.github.com>
2024-09-24 23:15:16 +0300

    Update route.ts

--- app/api/water/weekly/route.ts
+++ app/api/water/weekly/route.ts
@@ -1,54 +1,24 @@
-import { eachDayOfInterval, endOfDay, format, startOfDay, subDays } from 'date-fns';
-import groupBy from 'lodash/groupBy';
-import { NextResponse } from 'next/server';
-import { getServerSession } from 'next-auth/next';
+import { endOfWeek, startOfWeek } from 'date-fns';
+import { NextRequest, NextResponse } from 'next/server';
 
-import { authOptions } from '@/lib/authOptions';
 import prisma from '@/prisma/prisma';
 
-export async function GET() {
-    const session = await getServerSession(authOptions);
+export const GET = async (req: NextRequest) => {
+    const searchParams = req.nextUrl.searchParams;
+    const date = searchParams.get('date');
+    const parsedDate = new Date(date!);
 
-    if (!session) {
-        return NextResponse.json({ message: 'Not authenticated' }, { status: 401 });
-    }
+    const startDate = startOfWeek(parsedDate);
+    const endDate = endOfWeek(parsedDate);
 
-    const today = new Date();
-    const startDate = startOfDay(subDays(today, 6));
-    const endDate = endOfDay(today);
-
-    try {
-        const entries = await prisma.water.findMany({
-            where: {
-                userId: session.user.id,
-                date: {
-                    gte: startDate,
-                    lte: endDate,
-                },
+    const getDailyWater = await prisma.water.findMany({
+        where: {
+            date: {
+                gte: startDate,
+                lte: endDate,
             },
-        });
+        },
+    });
 
-        const groupedEntries = groupBy(entries, (entry) =>
-            format(new Date(entry.date), 'yyyy-MM-dd')
-        );
-
-        const dailyWaterIntake = eachDayOfInterval({ start: startDate, end: endDate }).map(
-            (day) => {
-                const formattedDay = format(day, 'yyyy-MM-dd');
-                const totalVolume = (groupedEntries[formattedDay] || []).reduce(
-                    (sum, { volume = 0 }) => sum + volume,
-                    0
-                );
-
-                return {
-                    date: formattedDay,
-                    totalWater: totalVolume,
-                };
-            }
-        );
-
-        return NextResponse.json(dailyWaterIntake);
-    } catch (error) {
-        return NextResponse.json({ message: 'Error fetching data', error }, { status: 500 });
-    }
-}
+    return NextResponse.json(getDailyWater);
+};
