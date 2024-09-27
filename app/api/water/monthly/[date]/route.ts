9736ba9bbe2286e090286f61290d779d384217bc
Dmytro <64677316+l-dmitry94@users.noreply.github.com>
2024-09-27 11:17:54 +0300

    fix

--- app/api/water/monthly/[date]/route.ts
+++ app/api/water/monthly/[date]/route.ts
@@ -1,35 +1,34 @@
-import { endOfDay, startOfDay } from 'date-fns';
+import { endOfMonth, startOfMonth } from 'date-fns';
 import { NextRequest, NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth';
 
 import { authOptions } from '@/lib/authOptions';
 import prisma from '@/prisma/prisma';
 
-export const GET = async (req: NextRequest) => {
+export const GET = async (req: NextRequest, { params }: { params: { date: string } }) => {
     const session = await getServerSession(authOptions);
 
     if (!session) {
         return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
     }
 
-    const searchParams = req.nextUrl.searchParams;
-    const date = searchParams.get('date');
+    const { date } = params;
 
-    const startDay = startOfDay(new Date(date!)).toISOString();
-    const endDay = endOfDay(new Date(date!)).toISOString();
+    const startMonth = startOfMonth(new Date(date!)).toISOString();
+    const endMonth = endOfMonth(new Date(date!)).toISOString();
 
     try {
-        const getDailyWater = await prisma.water.findMany({
+        const getMonthlyWater = await prisma.water.findMany({
             where: {
                 userId: session.user.id,
                 date: {
-                    gte: startDay,
-                    lte: endDay,
+                    gte: startMonth,
+                    lte: endMonth,
                 },
             },
         });
 
-        return NextResponse.json(getDailyWater);
+        return NextResponse.json(getMonthlyWater);
     } catch (error) {
         return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
     }
