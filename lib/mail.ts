e2aaa39d0d501e003a7d359174a88f20aea6cdc3
Dmytro <64677316+l-dmitry94@users.noreply.github.com>
2024-10-21 11:23:06 +0300

    update

--- lib/mail.ts
+++ lib/mail.ts
@@ -1,18 +1,31 @@
-import { Resend } from 'resend';
+import nodemailer from 'nodemailer';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const nodemailerConfig = {
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: process.env.GMAIL_EMAIL,
+        pass: process.env.GMAIL_PASS,
+    },
+};
 
-const domain = 'http://localhost:3000';
+const transport = nodemailer.createTransport(nodemailerConfig);
 
 const sendVerificationEmail = async (email: string, token: string) => {
+    const domain = process.env.DOMAIN_URL;
+
     const confirmationLink = `${domain}/verify-email?token=${token}`;
 
-    await resend.emails.send({
-        from: 'onboarding@resend.dev',
+    const verifyEmail = {
+        from: process.env.GMAIL_EMAIL,
         to: email,
         subject: 'Verify your email',
         html: `<p>Click <a href="${confirmationLink}">here</a> to verify your email</p>`,
-    });
+    };
+
+    return transport.sendMail(verifyEmail);
 };
 
 export default sendVerificationEmail;
